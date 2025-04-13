8997766590892b9a6967a8526c1800f6bc981f02
unknown <anvigollu@gmail.com>
2025-04-13 12:05:53 -0400

    some updates

--- frontend/src/pages/login/ShelterLogin.jsx
+++ frontend/src/pages/login/ShelterLogin.jsx
@@ -62,21 +62,21 @@ function ShelterSignup() {
       <h4>SHELTER NAME *</h4>
       <input
         type="text"
-        placeholder="shelter name"
+        placeholder="Shelter Name"
         value={shelterName}
         onChange={(e) => setShelterName(e.target.value)}
       />
       <h4>PASSWORD *</h4>
       <input
         type="password"
-        placeholder="password"
+        placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
       <h4>CONFIRM PASSWORD *</h4>
       <input
         type="password"
-        placeholder="reenter password"
+        placeholder="Reenter Password"
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)}
       />
@@ -89,11 +89,11 @@ function ShelterSignup() {
       />
       <h4 className="sltc">Terms and Conditions</h4>
       <p>
-        1. By using Meal Match, you agree to these Terms and Conditions. If you do not agree, you may not use the Platform. We will run a background check to confirm this is a real business to allow your account to be accepted.
-        2. MealMatch facilitates business connections by revealing contact email information once a match between businesses is confirmed. All further communication between parties occurs off-platform. To proceed with connections, users must interact with designated stages on the Platform.
-        3. Users must provide accurate information when signing up. Users are responsible for their communications after a match is made. The Platform is not liable for any interactions, agreements, or disputes that arise between matched parties.
-        4. Contact email information is only revealed upon mutual confirmation of a match.The Platform does not facilitate or monitor further communications between users.
-        5. MealMatch is not responsible for any damages, losses, or disputes resulting from user interactions after contact information has been shared.
+        1. By using Meal Match, you agree to these Terms and Conditions. If you do not agree, you may not use the Platform. We will run a background check to confirm this is a real business to allow your account to be accepted.<br />
+        2. MealMatch facilitates business connections by revealing contact email information once a match between businesses is confirmed. All further communication between parties occurs off-platform. To proceed with connections, users must interact with designated stages on the Platform.<br />
+        3. Users must provide accurate information when signing up. Users are responsible for their communications after a match is made. The Platform is not liable for any interactions, agreements, or disputes that arise between matched parties.<br />
+        4. Contact email information is only revealed upon mutual confirmation of a match.The Platform does not facilitate or monitor further communications between users.<br />
+        5. MealMatch is not responsible for any damages, losses, or disputes resulting from user interactions after contact information has been shared.<br />
         6. We reserve the right to modify these Terms and Conditions at any time. Continued use of the Platform constitutes acceptance of any changes.
       </p>
     </div>
